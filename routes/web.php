1aadf58ffd193b7d3e9332a3355c56e69fe31977
Agne <agnepetrylaite@gmail.com>
2017-05-24 12:28:55 +0300

    categories route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,21 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'admin'], function (){
+
+    Route::group(['prefix' => 'categories'], function (){
+        Route::get('/', ['as' => 'app.categories.index', 'uses' => 'VrCategoriesController@index']);
+        Route::get('/create', ['as' => 'app.categories.create', 'uses' => 'VrCategoriesController@create']);
+        Route::post('/create', [ 'uses' => 'VrCategoriesController@store']);
+
+        Route::group(['prefix' => '{id}'], function () {
+            Route::get('/', ['uses' => 'VrCategoriesController@show']);
+            Route::get('/edit', ['as' => 'app.categories.edit', 'uses' => 'VrCategoriesController@edit']);
+            Route::post('/edit', ['uses' => 'VrCategoriesController@update']);
+            Route::delete('/delete', ['as' => 'app.categories.destroy', 'uses' => 'VrCategoriesController@destroy']);
+
+        });
+    });
+});
