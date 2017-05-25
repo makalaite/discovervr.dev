f1ef442d8fced2035b17fbdefea3e5cd61cce017
Agne <agnepetrylaite@gmail.com>
2017-05-25 15:33:25 +0300

    category blade for adding

--- resources/views/admin/page/create.blade.php
+++ resources/views/admin/page/create.blade.php
@@ -1,5 +1,17 @@
 {!! Form::open(['url' => route('app.categories.create')]) !!}
 
-{{ Form::label('name', 'Insert category: ') }}
+{{ Form::label('name', 'Insert new category: ') }}
 {{ Form::text('name') }}
+
+
+
+{{--{{ Form::label('name', 'Insert new language: ') }}--}}
+{{--{{ Form::text('language_id') }}--}}
+
+{{ Form::label('language_code', 'Select a language: ') }}
+{{ Form::select('language_code', $lang) }}
+
+
+
 {{ Form::submit('Click Me!') }}
+{!! Form::close() !!}
