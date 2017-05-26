e267545ccdf3a0c9e74bbb4b2c89ea83840b7239
Agne <agnepetrylaite@gmail.com>
2017-05-26 15:14:35 +0300

    all categories list blade

--- resources/views/admin/page/list.blade.php
+++ resources/views/admin/page/list.blade.php
@@ -1,17 +1,8 @@
+@extends('admin.page.admin')
 
-<!DOCTYPE html>
-
-<html lang="en">
-<head>
-    <meta name="csrf-token" content="{{ csrf_token() }}"/>
-
-</head>
-<body style ="background-color:lightblue;">
-
-
-
+@section('content-list')
 <div class="container">
-    <table class="table table-bordered=1px">
+    <table class="table table-bordered">
         <thead>
         <tr>
             @foreach($list[0] as $key => $value)
@@ -51,9 +42,10 @@
         </tbody>
     </table>
 </div>
-</body>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</html>
+@endsection
+
 @section('scripts')
     <script>
         $.ajaxSetup({
