6669bb3a24d8d7c56a0ae4516aefffef4c95f2e2
Agne <agnepetrylaite@gmail.com>
2017-05-26 15:15:51 +0300

    form for creating catgeories

--- resources/views/admin/page/create.blade.php
+++ resources/views/admin/page/create.blade.php
@@ -1,15 +1,17 @@
+@extends('admin.page.admin')
+@include('layouts.style')
+
 {!! Form::open(['url' => route('app.categories.create')]) !!}
 
-{{ Form::label('name', 'Insert new category: ') }}
+{{ Form::label('name', 'Insert category name: ') }}
 {{ Form::text('name') }}
 
+{{ Form::label('name_en', 'Insert category name(en): ') }}
+{{ Form::text('name_en') }}
 
 
 {{--{{ Form::label('name', 'Insert new language: ') }}--}}
 {{--{{ Form::text('language_id') }}--}}
-
-{{ Form::label('language_code', 'Select a language: ') }}
-{{ Form::select('language_code', $lang) }}
 
 
 
