cb2931607bdf98ecf51f36bd2a1059ab2cadd19f
Agne <agnepetrylaite@gmail.com>
2017-05-26 15:14:55 +0300

    main admin blade

---
+++ resources/views/admin/page/admin.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    @include('layouts.style')
+
+
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+
+</head>
+<body style="background-color:lightblue;">
+
+@yield('content-list')
+
+
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</html>
+
+@yield('scripts')
